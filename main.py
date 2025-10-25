4ff96aa5e5b65bae5469d92f83ab108d04e15782
Whois <113191030+dratx1@users.noreply.github.com>
2025-10-25 19:03:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ theme = get_theme()
 def show_main_menu(profile):
     expired_at_dt = datetime.fromtimestamp(profile["balance_expired_at"]).strftime("%Y-%m-%d")
     pulsa_str = get_rupiah(profile["balance"])
-    theme = get_theme()
+    get_theme()
 
     info_table = Table.grid(padding=(0, 1))
     info_table.add_column(justify="left", style=theme["text_body"])
