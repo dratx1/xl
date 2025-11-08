b6648b0970631c194fb75bd180dbca968f0d5683
Whois <113191030+dratx1@users.noreply.github.com>
2025-11-08 17:31:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def run_main():
         import master
         master.main()
     except KeyboardInterrupt:
-        print_panel("👋 Keluar", "Aplikasi dihentikan oleh pengguna", border_style=get_theme_style("border_warning"))
+        print_panel("👋 Keluar", "Aplikasi dihentikan oleh pengguna", border_style=get_theme_style("border_info"))
         #pause()
         sys.exit(0)
     except Exception as e:
