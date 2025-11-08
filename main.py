992a566224e4c2b8b5ca9146179cf6cca0385a68
Whois <113191030+dratx1@users.noreply.github.com>
2025-11-08 20:18:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def run_main():
         print_panel("👋 Keluar", "Aplikasi dihentikan oleh pengguna", border_style=get_theme_style("border_info"))
         sys.exit(0)
     except Exception as e:
-        print_error("❌ Gagal menjalankan master.py", f"{type(e).__name__} - {e}")
+        print_error("❌ Gagal menjalankan menu utama", f"{type(e).__name__} - {e}")
         pause()
         sys.exit(1)
 
